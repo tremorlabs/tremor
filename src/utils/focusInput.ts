ba68ed59c850d4c60d14f258d9509e39045d0ba4
severinlandolt <sev.landolt@gmail.com>
2024-03-26 21:23:55 +0100

    fix: update order inputs

--- src/utils/focusInput.ts
+++ src/utils/focusInput.ts
@@ -1,4 +1,10 @@
-// Tremor Raw focusInput [v0.0.0]
+// Tremor Raw focusInput [v0.0.1]
 
-export const focusInput =
-  "focus:ring-2 focus:ring-blue-200 focus:border-blue-500 focus:dark:ring-blue-700/30 focus:dark:border-blue-700"
+export const focusInput = [
+  // base
+  "focus:ring-2",
+  // ring color
+  "focus:ring-blue-200 focus:dark:ring-blue-700/30",
+  // border color
+  "focus:border-blue-500 focus:dark:border-blue-700",
+]
