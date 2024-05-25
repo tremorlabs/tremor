a5b9ab2d6916fa1bb37f04599a888b5bcf258a0e
severinlandolt <sev.landolt@gmail.com>
2024-05-25 15:15:31 +0200

    update util

--- src/utils/hasOnlyOneValueForKey.ts
+++ src/utils/hasOnlyOneValueForKey.ts
@@ -1,7 +1,10 @@
 // Tremor Raw hasOnlyOneValueForKey [v0.1.0]
 
-export function hasOnlyOneValueForKey(array: any[], keyToCheck: string) {
-  const val = []
+export function hasOnlyOneValueForKey(
+  array: any[],
+  keyToCheck: string,
+): boolean {
+  const val: any[] = []
 
   for (const obj of array) {
     if (Object.prototype.hasOwnProperty.call(obj, keyToCheck)) {
