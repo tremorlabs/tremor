607a3623cae472aad7d4dd1fd1ef8395323066a2
severinlandolt <sev.landolt@gmail.com>
2024-06-19 18:11:57 +0200

    update tests

--- src/components/Tracker/tracker.spec.ts
+++ src/components/Tracker/tracker.spec.ts
@@ -23,7 +23,7 @@ test.describe("Expect tracker default", () => {
     await expect(
       page
         .frameLocator('iframe[title="storybook-preview-iframe"]')
-        .locator(".h-full")
+        .locator(".size-full > .size-full")
         .first(),
     ).toBeVisible()
   })
