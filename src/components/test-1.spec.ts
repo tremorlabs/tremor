a36d0481e6511a855b7e4d4f46aa5525d87215cd
severinlandolt <sev.landolt@gmail.com>
2024-09-03 00:33:34 +0200

    remove test file

--- src/components/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:6006/?path=/story/ui-datepicker--single');
-  await page.getByRole('button', { name: 'Hide addons [⌥ A]' }).click();
-});
