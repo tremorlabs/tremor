ce86ef4a1d35e780872587fa4c6915198f8dce41
Severin Landolt <41927988+severinlandolt@users.noreply.github.com>
2024-11-03 15:01:55 +0100

    Chore/storybook (#98)
    * chore: update storybook
    
    * update addons
    
    * fix: linting
    
    * update packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
