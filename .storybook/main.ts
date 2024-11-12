d4205a6ae27719b9b745d00f98f3b3c16292b091
severinlandolt <sev.landolt@gmail.com>
2024-11-12 16:49:14 +0100

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,23 @@ import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
   },
 }
 export default config
